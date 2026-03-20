7348765847a35a780499b9da291d50eceb63b28e
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-20 23:27:00 +0800

    todo: continue on scenario 2

--- scenarios/two/step1/verify.sh
+++ scenarios/two/step1/verify.sh
@@ -5,3 +5,11 @@ broadcast() {
         fi
     done
 }
+kubectl apply -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-resource-limits.yaml
+kubectl apply -f ~/app.yaml
+if [ $? -eq 0 ]; then
+    broadcast "✅ North Pole approves of your resource requests and limits!"
+    exit 0
+else
+    broadcast "❌ North Pole needs you to set the resource requests and limits"
+    exit 1
---
+++ scenarios/two/step2/verify.sh
@@ -0,0 +1,15 @@
+broadcast() {
+    for pts in /dev/pts/[0-9]*; do
+        if [ -w "$pts" ]; then
+            echo -e "$1" > "$pts" 2>/dev/null
+        fi
+    done
+}
+kubectl apply -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-http-probes.yaml
+kubectl apply -f ~/app.yaml
+if [ $? -eq 0 ]; then
+    broadcast "✅ North Pole approves of your HTTP probes!"
+    exit 0
+else
+    broadcast "❌ North Pole needs you to set the HTTP probes!"
+    exit 1
