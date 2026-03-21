1f8544017553fb788f799863c180c77b95141311
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-21 22:02:54 +0800

    fix: background script

--- scenarios/two/step1/background.sh
+++ scenarios/two/step1/background.sh
@@ -1,3 +1,4 @@
 ## Apply Resource Limits
+kubectl delete -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-resource-limits.yaml
 sleep 5
 touch /tmp/setup-finished
