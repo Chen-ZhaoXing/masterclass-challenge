7c6f04f4522ee4e84abc7f3dacf40dfb49a733eb
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-23 06:21:26 +0800

    fix: scenarior

--- scenarios/two/step1/verify.sh
+++ scenarios/two/step1/verify.sh
@@ -6,7 +6,7 @@ broadcast() {
     done
 }
 kubectl apply -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-resource-limits.yaml
-kubectl apply -f ~/app.yaml
+kubectl apply -f app.yaml
 if [ $? -eq 0 ]; then
     broadcast "✅ North Pole approves of your resource requests and limits!"
     exit 0
--- scenarios/two/step2/background.sh
+++ scenarios/two/step2/background.sh
@@ -1,6 +1,9 @@
 ## Delete and apply the new policy
 kubectl delete -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-resource-limits.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-http-probes.yaml
+
 curl https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-deployments/app.yaml > ~/app.yaml
+
 sleep 5
 touch /tmp/setup-finished
