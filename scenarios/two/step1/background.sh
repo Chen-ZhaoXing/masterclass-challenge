413bcba05263ff5809047fe555f4ab88650269f6
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-23 09:10:56 +0800

    fix

--- scenarios/two/step1/background.sh
+++ scenarios/two/step1/background.sh
@@ -1,2 +1,11 @@
-kubectl apply -f /var/kyverno-policies/require-resource-limits.yaml --force
+broadcast() {
+    for pts in /dev/pts/[0-9]*; do
+        if [ -w "$pts" ]; then
+            echo -e "$1" > "$pts" 2>/dev/null
+        fi
+    done
+}
+
+kubectl apply -f /var/kyverno-policies/require-resource-limits.yaml
 touch /tmp/setup-finished
+broadcast "✅ The Lab is now ready!"
--- scenarios/two/step2/background.sh
+++ scenarios/two/step2/background.sh
@@ -1,4 +1,4 @@
 ## Delete and apply the new policy
-kubectl apply -f /var/kyverno-policies/require-http-probes.yaml --force
+kubectl apply -f /var/kyverno-policies/require-http-probes.yaml
 sleep 10
 touch /tmp/setup-finished
