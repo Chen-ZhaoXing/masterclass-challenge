25761e52eb274532acf78af2b1e7673a53755872
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-23 09:18:20 +0800

    fix

--- scenarios/two/background.sh
+++ scenarios/two/background.sh
@@ -13,8 +13,8 @@ sleep 15
 
 # Wait for Kyverno to be ready
 kubectl wait --for=condition=ready pod -l app=admission -n kyverno --timeout=300s
-kubectl apply -f https://raw.githubusercontent.com/touching123/masterclass-challenge-assets/refs/heads/main/kyverno-policies/require-resource-limits.yaml
 kubectl create ns gift-tracking
+kubectl apply -f /var/kyverno-policies/require-resource-limits.yaml
 kubectl config set-context --current --namespace=gift-tracking
 
 
