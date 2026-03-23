7b2c13c63c0a055ce564778ad9aff95483773535
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-23 08:37:04 +0800

    fix

--- scenarios/two/step1/verify.sh
+++ scenarios/two/step1/verify.sh
@@ -9,9 +9,10 @@ kubectl apply -f /var/kyverno-policies/require-resource-limits.yaml >/dev/null 2
 APPLY_OUT=$(kubectl apply --dry-run=server -f ~/app.yaml 2>&1)
 if [ $? -eq 0 ]; then
     broadcast "✅ North Pole approves of your resource requests and limits!"
+    sleep 5
     exit 0
 else
     broadcast "❌ North Pole needs you to set the resource requests and limits"
+    sleep 5
     exit 1
 fi
-sleep 15
