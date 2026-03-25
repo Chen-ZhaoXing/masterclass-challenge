dd87ca49ddcf45fdfd61add971172c250add2963
Yu Rong <yu_rong123@yahoo.com.sg>
2026-03-25 19:01:05 +0800

    fix

--- scenarios/trojan-manifest/step2/background.sh
+++ scenarios/trojan-manifest/step2/background.sh
@@ -1,2 +1,3 @@
 ## Delete and apply the new policy
 kubectl apply -f /var/kyverno-policies/require-http-probes.yaml
+touch /tmp/setup-finished
--- scenarios/trojan-manifest/step4/background.sh
+++ scenarios/trojan-manifest/step4/background.sh
@@ -1,1 +1,2 @@
 kubectl apply -f /var/kyverno-policies/require-non-default-sa.yaml
+touch /tmp/setup-finished
